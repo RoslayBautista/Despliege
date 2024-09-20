4752d0da2f2d6b05dc9a88efbcc2768487272fcc
RoslayBautista <roslaymaryeline@gmail.com>
2024-09-20 05:38:27 -0500

    instalacion mysql2 y body-parser

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const config = require('./config'); // Importa la configuración
 const multer = require('multer');
 const path = require('path');
