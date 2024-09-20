560abfd3900a920beefc711536e67970a0a09655
RoslayBautista <roslaymaryeline@gmail.com>
2024-09-20 05:17:39 -0500

    cambio de variables de ntorno railwai

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js
 
 // Verifica si las variables de entorno están definidas
-const DB_HOST = process.env.DB_HOST || 'localhost'; // Valor por defecto
+const DB_HOST = process.env.DB_HOST || 'mysql.railway.internal'; // Valor por defecto
 const DB_USER = process.env.DB_USER || 'root'; // Valor por defecto
-const DB_PASSWORD = process.env.DB_PASSWORD || '';
-const DB_NAME = process.env.DB_NAME || 'nodejs'; // Valor por defecto
-const DB_PORT = process.env.DB_PORT || 3307; // Agregar el puerto
+const DB_PASSWORD = process.env.DB_PASSWORD || 'avicAhgYwUNXHpKbzLblJKAejpYVGGtB';
+const DB_NAME = process.env.DB_NAME || 'railway'; // Valor por defecto
+const DB_PORT = process.env.DB_PORT || 3306; // Agregar el puerto
 
 
 module.exports = {
